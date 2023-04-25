6c2dd62fb911090ef472b236908d797b9966e0e8
osmangony01 <mdosmangoni376@gmail.com>
2023-04-25 13:16:29 +0600

    make responsive header section

---
+++ Js/toggle-menu.js
@@ -0,0 +1,11 @@
+
+const toggleMenu = () => {
+    let x = document.getElementById("mobile-menu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+
+}
+
